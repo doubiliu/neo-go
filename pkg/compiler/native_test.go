b04ed131a4688e04386d6e7983ea3e9d86bdb5ad
Anna Shaleva <shaleva.ann@nspcc.ru>
2021-03-24 13:16:38 +0300

    core: move NNS record types to a separate package
    We need this to avoid `native` dependency in RPC client.

--- pkg/compiler/native_test.go
+++ pkg/compiler/native_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/nspcc-dev/neo-go/pkg/core/interop/interopnames"
 	"github.com/nspcc-dev/neo-go/pkg/core/native"
 	"github.com/nspcc-dev/neo-go/pkg/core/native/noderoles"
+	"github.com/nspcc-dev/neo-go/pkg/core/native/recordtypes"
 	"github.com/nspcc-dev/neo-go/pkg/interop/native/crypto"
 	"github.com/nspcc-dev/neo-go/pkg/interop/native/gas"
 	"github.com/nspcc-dev/neo-go/pkg/interop/native/ledger"
@@ -77,10 +78,10 @@ func TestRoleManagementRole(t *testing.T) {
 }
 
 func TestNameServiceRecordType(t *testing.T) {
-	require.EqualValues(t, native.RecordTypeA, nameservice.TypeA)
-	require.EqualValues(t, native.RecordTypeCNAME, nameservice.TypeCNAME)
-	require.EqualValues(t, native.RecordTypeTXT, nameservice.TypeTXT)
-	require.EqualValues(t, native.RecordTypeAAAA, nameservice.TypeAAAA)
+	require.EqualValues(t, recordtypes.A, nameservice.TypeA)
+	require.EqualValues(t, recordtypes.CNAME, nameservice.TypeCNAME)
+	require.EqualValues(t, recordtypes.TXT, nameservice.TypeTXT)
+	require.EqualValues(t, recordtypes.AAAA, nameservice.TypeAAAA)
 }
 
 func TestCryptoLibNamedCurve(t *testing.T) {
